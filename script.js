c2d2d8a8a821306db7bbd8ff2d4729393fbb36f3
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2024-10-23 21:56:56 -0300

    chore: new changes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // Handle flipping the card when "Learn More" is clicked
-document.querySelector('.learn-more').addEventListener('click', function(event) {
-  event.stopPropagation();
-  document.querySelector('.card').classList.add('flip');
+document.querySelectorAll('.learn-more').forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    const card = this.closest('.card'); // Find the closest card to the clicked button
+    card.classList.add('flip');
+  });
 });
 
 // Handle flipping back the card when "X" is clicked
-document.querySelector('.close-btn').addEventListener('click', function(event) {
-  event.stopPropagation();
-  document.querySelector('.card').classList.remove('flip');
+document.querySelectorAll('.close-btn').forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.stopPropagation();
+    const card = this.closest('.card'); // Find the closest card to the clicked button
+    card.classList.remove('flip');
+  });
 });
